b6630345b5bf551847f07acfcc4655c9732059f4
MARIO USANGA <mariobomausanga@gmail.com>
2024-08-07 16:47:47 +0100

    fifth commit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
           <h1 className='feel-free'>Feel free to contact me</h1>
           <div className='contact-list'>
             <a href='https://www.figma.com/design/sKeyLqz7T08DONuI2nXOO0/Portfolio?m=auto&t=0KcVFOfYs0EfCsLX-6'><img src={Figma} className="icon"></img></a>
-            <a href='mailto:mariousangaboma@gmail.com'><img src={Gmail} className="icon"></img></a>
+            <a href='mailto:mariobomausanga@gmail.com'><img src={Gmail} className="icon"></img></a>
             <a href='https://github.com/MAX1NITY'><img src={Github} className="icon"></img></a>
           </div>
         </div>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
             </div>
             <div className="cont">
               <a href='https://www.figma.com/design/sKeyLqz7T08DONuI2nXOO0/Portfolio?m=auto&t=0KcVFOfYs0EfCsLX-6'><img src={Figma} className="icon"></img></a>
-              <a href='mailto:mariousangaboma@gmail.com'><img src={Gmail} className="icon"></img></a>
+              <a href='mailto:mariobomausanga@gmail.com'><img src={Gmail} className="icon"></img></a>
               <a href='https://github.com/MAX1NITY'><img src={Github} className="icon"></img></a>
             </div>
           </div>
