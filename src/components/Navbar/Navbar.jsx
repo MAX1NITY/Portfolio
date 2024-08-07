1cbaa9a14a226457bf2001875cd0faedfaa9cd57
MARIO USANGA <mariobomausanga@gmail.com>
2024-08-07 16:20:42 +0100

    fourth commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Navbar.css"
 import Logo from "./Logo.svg"
 import List from "./List.svg"
+import Closeicon from "./Closeicon.svg"
 
 const Navbar = () => {
+  const [isActive, setIsActive] = useState(false);
+
   return (
+    <>
     <nav>
        <a href='#about' className='logo'><img src={Logo} className="logo-img"></img>MARIO</a>
 
@@ -26,10 +30,37 @@ const Navbar = () => {
       </li>
       
     </ul>
-    <div className='menu-icon'>
+    <div onClick={() => setIsActive(true)} className='menu-icon'>
       <img src={List} className='List'></img>
     </div>
     </nav>
+
+    <div className={`mobile-menu-cont ${isActive ? "active" : ""}`}>
+      <div onClick={() => setIsActive(false)} className='close-icon'>
+        <img src={Closeicon} />
+      </div>
+      
+      <ul className='menu-items'>
+      <li>
+        <a onClick={() => setIsActive(false)} href='#'>Home</a>
+      </li>
+
+      <li>
+        <a onClick={() => setIsActive(false)} href='#about'>About</a>
+      </li>
+
+      <li>
+        <a onClick={() => setIsActive(false)} href='#work'>Work</a>
+      </li>
+
+      <li>
+        <a onClick={() => setIsActive(false)} href='#contact'>Contact</a>
+      </li>
+      
+    </ul>
+    </div>
+
+    </>
   )
 }
 
