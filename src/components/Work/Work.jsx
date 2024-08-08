c4a2b295b83835debc2fd06f8e85d6959f09cd5d
MARIO USANGA <mariobomausanga@gmail.com>
2024-08-08 17:44:02 +0100

    second commit

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -11,31 +11,31 @@ const Work = () => {
         <h1 className='work-h'>WORK</h1>
         <div className='projects'>
           <div className='column'>
-            <a href=''>
+            <a href='https://max1nity-github-io.vercel.app/'>
             <img src={Profile} className="profile-img"></img>
             </a>
 
-            <a href=''>
+            <a href='https://max1nity-github-io.vercel.app/'>
             <p className='profile-p'>Personal Project - PROFILE</p>
             </a>
           </div>
 
           <div className='column'>
-            <a href=''>
+            <a href='https://fashion-proj.vercel.app/'>
             <img src={Fashion} className="profile-img"></img>
             </a>
 
-            <a href=''>
+            <a href='https://fashion-proj.vercel.app/'>
             <p className='profile-p'>Personal Project - Fashion</p>
             </a>
           </div>
 
           <div className='column'>
-            <a href=''>
+            <a href='/'>
             <img src={Arrow} className="profile-img"></img>
             </a>
 
-            <a href=''>
+            <a href='/'>
             <p className='profile-p'>Personal Project - Portfolio</p>
             </a>
           </div>
